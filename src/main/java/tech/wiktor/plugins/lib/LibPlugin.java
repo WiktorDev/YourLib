02b7f8de0c24d849d1f174cfb924d7bfef2f1438
WiktorDev <mcserw4@wp.pl>
2022-07-30 15:55:22 +0200

    first commit

---
+++ src/main/java/tech/wiktor/plugins/lib/LibPlugin.java
@@ -0,0 +1,17 @@
+package tech.wiktor.plugins.lib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LibPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
